e3f8d0781d840a21ddc803c66b7b6e13d7c0a00d
moliloo <jhjmbrito@gmail.com>
2024-04-01 18:02:08 -0300

    adding comments to explain to my professors who will read my homework

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+// como estou utilizando um header com posição fixa, quando clicava numa âncora numa sessão,
+// o título ficava atrás do header, a função desse scrip serve apenas para faça uma pequena
+// animação para ir a sessão clicada e que ele fique abaixo do header, não embaixo
+
 document.querySelectorAll('.link-secao').forEach((anchor) => {
 	anchor.addEventListener('click', function (e) {
 		e.preventDefault()
